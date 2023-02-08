cc5b3f58d2264dac08c41f420caed05c92290706
AlvardBarseghyan <barseghyan.at@gmail.com>
2023-02-08 16:09:31 +0400

    pl_train and dataset.py files adapted to nightowls

--- pl_train.py
+++ pl_train.py
@@ -87,7 +87,7 @@ class LightningMAE(pl.LightningModule):
 
         if self.min_loss > total_loss:
             self.min_loss = total_loss.item()
-            torch.save(self.model_mae.state_dict(), "/mnt/2tb/alla/mae/mae_contastive/custom_cosine_sim/best_model.pth")
+            torch.save(self.model_mae.state_dict(), "/mnt/2tb/alla/mae/mae_contastive/nightowls/best_model.pth")
 
         return total_loss
 
@@ -104,10 +104,10 @@ class LightningMAE(pl.LightningModule):
 if __name__ == '__main__':
 
     #### init dataset ####
-    root = '/mnt/2tb/hrant/FAIR1M/fair1m_1000/train1000/'
-    path_ann = os.path.join(root, 'few_shot_8.json')
-    path_imgs = os.path.join(root, 'images')
-    dataset = MAEDataset(path_ann, path_imgs, resize_image=True)
+
+    path_ann = '/home/barseghyan/Projects/GLIP/annotations/nightowls/val/annotations_nightowls_coco.json'
+    path_imgs = '/mnt/2tb/hrant/nightowls/val'
+    dataset = MAEDataset(path_ann, path_imgs, intersection_threshold=0.01, resize_image=True)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
 
     #### init model ####
@@ -125,13 +125,13 @@ if __name__ == '__main__':
         chkpt_dir = '/mnt/2tb/hrant/checkpoints/mae_models/mae_visualize_vit_large_ganloss.pth'
         checkpoint = torch.load(chkpt_dir, map_location='cuda')
         msg = model_mae.load_state_dict(checkpoint['model'], strict=False)
-        chkpt_dir = '/mnt/2tb/alla/mae/mae_contastive/custom_cosine_sim/lightning_logs/version_5/checkpoints/epoch=15-step=16.ckpt'
-        model_mae = LightningMAE.load_from_checkpoint(chkpt_dir, model=model_mae)
-        model_mae = model_mae.model_mae
+        # chkpt_dir = '/mnt/2tb/alla/mae/mae_contastive/custom_cosine_sim/lightning_logs/version_5/checkpoints/epoch=15-step=16.ckpt'
+        # model_mae = LightningMAE.load_from_checkpoint(chkpt_dir, model=model_mae)
+        # model_mae = model_mae.model_mae
 
 
-    model = LightningMAE(model_mae, l1=1)
+    model = LightningMAE(model_mae, num_classes=3, lr=0.0001, l1=1)
     trainer = pl.Trainer(logger=True, enable_checkpointing=True, limit_predict_batches=BATCH_SIZE, max_epochs=EPOCHS, log_every_n_steps=1, \
-        default_root_dir="/mnt/2tb/alla/mae/mae_contastive/custom_cosine_sim", ) #, accelerator='gpu',\
+        default_root_dir="/mnt/2tb/alla/mae/mae_contastive/nightowls", ) #, accelerator='gpu',\
         #  devices=1, )
     trainer.fit(model=model, train_dataloaders=dataloader)
