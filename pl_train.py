f0f4ba65257ed89258f0e64fe93e830a084100e3
AlvardBarseghyan <barseghyan.at@gmail.com>
2023-02-10 18:46:06 +0400

    trying to add loger

--- pl_train.py
+++ pl_train.py
@@ -3,13 +3,39 @@ import torch
 import torch.nn as nn
 import pytorch_lightning as pl
 
+# from aim import Run
+from aim.pytorch_lightning import AimLogger
+
 from dataset import MAEDataset
 import models_mae
 
-BATCH_SIZE = 40
-EPOCHS = 500
-continue_from_checkpoint = True
+BATCH_SIZE = 20
+EPOCHS = 100
+RANDOM_INIT = True
+continue_from_checkpoint = False
+DEVICE = 'cpu'
+learning_rate = 1e-4
+l1 = 1
+intersection_threshold = 0.1
+# torch.cuda.empty_cache()
 
+
+# aim_logger = AimLogger(experiment='wt_background_random_init')
+# aim_logger.log_hyperparams({
+#     "max_epochs": 10,
+#     # 'learning_rate': learning_rate,
+#     # 'negative_loss_coef': l1,
+#     # 'class_intersection_threshold':intersection_threshold,
+#     # 'batch_size':BATCH_SIZE,
+#     # 'random_init':RANDOM_INIT
+# })
+
+# Run['hparams'] = {
+#     'learning_rate': learning_rate,
+#     'negative_loss_coef': l1,
+#     'class_intersection_threshold':intersection_threshold,
+#     'batch_size':BATCH_SIZE,
+#     'random_init':RANDOM_INIT}
 
 def cosine_distance_torch(x1, x2=None):
     x2 = x1 if x2 is None else x2
@@ -38,11 +64,13 @@ class ContrastiveLoss(nn.Module):
 
         positive_loss = (1 - distance_matrix) * cos_dist
         positive_loss /= (1 - distance_matrix).sum()
+        positive_loss = torch.nan_to_num(positive_loss)
 
         delta = self.margin - cos_dist # if margin == 1, then 1 - cos_dist == cos_sim
         delta= torch.clamp(delta, min=0.0, max=None)
         negative_loss = distance_matrix * delta
         negative_loss /= (distance_matrix).sum()
+        negative_loss = torch.nan_to_num(negative_loss)
 
         agg_loss = torch.zeros((self.num_classes+1, self.num_classes+1))
         agg_d = torch.zeros((self.num_classes+1, self.num_classes+1))
@@ -67,23 +95,39 @@ class LightningMAE(pl.LightningModule):
         self.lr = lr
         self.min_loss = 10
         self.criterion = ContrastiveLoss(num_classes=num_classes, margin=margin)
+        # self.logger = AimLogger(experiment='wt_background_random_init')
+        # self.logger.log_hyperparams({
+        #     "max_epochs": 10,
+        #     # 'learning_rate': learning_rate,
+        #     # 'negative_loss_coef': l1,
+        #     # 'class_intersection_threshold':intersection_threshold,
+        #     # 'batch_size':BATCH_SIZE,
+        #     # 'random_init':RANDOM_INIT
+        # })
         # self.save_hyperparameters()
 
     def training_step(self, batch, batch_idx):
-
+        # torch.cuda.empty_cache()
+        # print("Memory allocation init:", torch.cuda.memory_allocated(DEVICE) / 1024 / 1024 / 1024)
+        # print("Max memory allocation init:", torch.cuda.max_memory_allocated(DEVICE) / 1024 / 1024 / 1024)
         img = torch.einsum('nhwc->nchw', batch['image'])
-        print("Getting image embeddings...")
+        # print("Getting image embeddings...")
         img_enc, mask, _ = self.model_mae.forward_encoder(img.float(), mask_ratio=0)
         img_enc = img_enc[:, 1:, :].reshape(-1, img_enc.shape[-1])
-        print("Loss counting...")
+        # print("Memory allocation after embeddings:", torch.cuda.memory_allocated(DEVICE) / 1024 / 1024 / 1024)
+        # print("Max memory allocation after embeddings:", torch.cuda.max_memory_allocated(DEVICE) / 1024 / 1024 / 1024)
+        # print("Loss counting...")
         loss = self.criterion(img_enc, batch['indices_labels'].reshape(-1))
         total_loss = loss[0] + self.l1 * loss[1]
         self.log('train_loss', total_loss)
-        print(f'Iter: {batch_idx}, pos_loss: {loss[0].item()}, neg_loss = {self.l1} * {loss[1].item()}, loss: {total_loss.item()}')
+        # print("Memory allocation after loss:", torch.cuda.memory_allocated(DEVICE) / 1024 / 1024 / 1024)
+        # print("Max memory allocation after loss:", torch.cuda.max_memory_allocated(DEVICE) / 1024 / 1024 / 1024)
+        print()
+        # print(f'Iter: {batch_idx}, pos_loss: {loss[0].item()}, neg_loss = {self.l1} * {loss[1].item()}, loss: {total_loss.item()}')
 
         if self.min_loss > total_loss:
             self.min_loss = total_loss.item()
-            torch.save(self.model_mae.state_dict(), "/mnt/2tb/alla/mae/mae_contastive/custom_cosine_sim/best_model.pth")
+            torch.save(self.model_mae.state_dict(), "/mnt/2tb/alla/mae/mae_contastive/background_random_init/best_model.pth")
 
         return total_loss
 
@@ -103,7 +147,7 @@ if __name__ == '__main__':
     root = '/mnt/2tb/hrant/FAIR1M/fair1m_1000/train1000/'
     path_ann = os.path.join(root, 'few_shot_8.json')
     path_imgs = os.path.join(root, 'images')
-    dataset = MAEDataset(path_ann, path_imgs, intersection_threshold=0.1, resize_image=True)
+    dataset = MAEDataset(path_ann, path_imgs, intersection_threshold=intersection_threshold, resize_image=True)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
 
     #### init model ####
@@ -113,7 +157,7 @@ if __name__ == '__main__':
     if continue_from_checkpoint:
 
         chkpt_dir = '/mnt/2tb/hrant/checkpoints/mae_models/mae_visualize_vit_large_ganloss.pth'
-        checkpoint = torch.load(chkpt_dir, map_location='cuda')
+        checkpoint = torch.load(chkpt_dir, map_location=DEVICE)
         msg = model_mae.load_state_dict(checkpoint['model'], strict=False)
         chkpt_dir = '/mnt/2tb/alla/mae/mae_contastive/background/lightning_logs/version_1/checkpoints/epoch=142-step=143.ckpt'
         model_mae = LightningMAE.load_from_checkpoint(chkpt_dir, model=model_mae)
@@ -122,13 +166,14 @@ if __name__ == '__main__':
     else:
         # chkpt_dir = '/mnt/2tb/hrant/checkpoints/mae_models/mae_visualize_vit_large.pth'
         chkpt_dir = '/mnt/2tb/hrant/checkpoints/mae_models/mae_visualize_vit_large_ganloss.pth'
-        checkpoint = torch.load(chkpt_dir, map_location='cuda')
-        msg = model_mae.load_state_dict(checkpoint['model'], strict=False)
+        checkpoint = torch.load(chkpt_dir, map_location=DEVICE)
+        if not RANDOM_INIT:
+            msg = model_mae.load_state_dict(checkpoint['model'], strict=False)
         
 
 
-    model = LightningMAE(model_mae, l1=1)
+    model = LightningMAE(model_mae, lr=learning_rate, l1=l1)
     trainer = pl.Trainer(logger=True, enable_checkpointing=True, limit_predict_batches=BATCH_SIZE, max_epochs=EPOCHS, log_every_n_steps=1, \
-        default_root_dir="/mnt/2tb/alla/mae/mae_contastive/background",  ) #, accelerator='gpu',\
-        #  devices=1, )
+        default_root_dir="/mnt/2tb/alla/mae/mae_contastive/background_random_init", accelerator=DEVICE,) # devices=1,) # gpus=-1)
+    # trainer = pl.Trainer(logger=AimLogger(experiment='experiment_name'), accelerator=DEVICE, devices=1,)
     trainer.fit(model=model, train_dataloaders=dataloader)
